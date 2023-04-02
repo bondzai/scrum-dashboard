357eed96a10056b7ed37a0e99f534563b5d9c83d
JamesBond <introbond.dev@gmail.com>
2023-04-02 13:52:58 +0700

    minor change: rebrand

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         <nav className="navbar">
             <div className="container">
                 <div className="logo">
-                    {/* <AiFillCode /> */}
+                    {/* <Brand /> */}
                 </div>
                 <div className="menu-icon" onClick={handleShowNavbar}>
                     <GiHamburgerMenu />
