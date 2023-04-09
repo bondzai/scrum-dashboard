e99a261578ea9758d9d9d7648540d4675f9a2a8a
JamesBond <introbond.dev@gmail.com>
2023-04-09 11:02:50 +0700

    remove: export

--- src/pages/Scrum.jsx
+++ src/pages/Scrum.jsx
@@ -6,7 +6,6 @@ const Scrum = () => {
     return (
         <div className="home">
             <div className="about">
-                <h1> DAILY SCRUM DATA </h1>
                 <ScrumTable />
             </div>
         </div>
